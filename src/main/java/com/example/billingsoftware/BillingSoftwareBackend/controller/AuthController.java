28df11eb65ebb2a9bfb6b8720638453981a9005b
VarunSoni01 <varunsoni448@gmail.com>
2025-10-09 09:48:49 +0530

    Created login, aunthentication, encoding api's

---
+++ src/main/java/com/example/billingsoftware/BillingSoftwareBackend/controller/AuthController.java
@@ -0,0 +1,55 @@
+package com.example.billingsoftware.BillingSoftwareBackend.controller;
+
+import com.example.billingsoftware.BillingSoftwareBackend.io.AuthRequest;
+import com.example.billingsoftware.BillingSoftwareBackend.io.AuthResponse;
+import com.example.billingsoftware.BillingSoftwareBackend.service.implementation.AppUserDetailsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    private final AppUserDetailsService appUserDetailsService;
+
+    @PostMapping("/encode")
+    public String encodePassword(@RequestBody Map<String, String> request){
+        return passwordEncoder.encode(request.get("password"));
+    }
+
+    @PostMapping("/login")
+    public AuthResponse login(@RequestBody AuthRequest request) throws Exception{
+        authenticate(request.getEmail(),request.getPassword());
+        final UserDetails userDetails = appUserDetailsService.loadUserByUsername(request.getEmail());
+
+    }
+
+    private void authenticate(String email, String password)  throws Exception{
+        try{
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(email, password));
+        }catch (DisabledException e){
+           throw new Exception("User disabled");
+        }catch (BadCredentialsException e){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Email or password is incorrect");
+        }catch (Exception e){
+            throw new Exception("Exception found");
+        }
+    }
+
+
+}
--- src/main/java/com/example/billingsoftware/BillingSoftwareBackend/io/AuthResponse.java
+++ src/main/java/com/example/billingsoftware/BillingSoftwareBackend/io/AuthResponse.java
@@ -1,4 +1,12 @@
 package com.example.billingsoftware.BillingSoftwareBackend.io;
 
+import lombok.*;
+
+@Getter
+@AllArgsConstructor
+
 public class AuthResponse {
+    private String email;
+    private String token;
+    private String role;
 }
