157052adb46e92048726660110badade25543f7d
VarunSoni01 <varunsoni448@gmail.com>
2025-10-21 12:55:18 +0530

    Order controller created

---
+++ src/main/java/com/example/billingsoftware/BillingSoftwareBackend/controller/OrderController.java
@@ -0,0 +1,37 @@
+package com.example.billingsoftware.BillingSoftwareBackend.controller;
+
+import com.example.billingsoftware.BillingSoftwareBackend.io.OrderRequest;
+import com.example.billingsoftware.BillingSoftwareBackend.io.OrderResponse;
+import com.example.billingsoftware.BillingSoftwareBackend.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/orders")
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+
+    @PostMapping("/add")
+    @ResponseStatus(HttpStatus.CREATED)
+    public OrderResponse createOrder(@RequestBody OrderRequest request){
+        return orderService.createOrder(request);
+    }
+
+    @PostMapping("/delete/{orderId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteOrder(@PathVariable String orderId){
+        orderService.deleteOrder(orderId);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<OrderResponse> getLatestOrders(){
+        return orderService.getLatestOrders();
+    }
+
+}
