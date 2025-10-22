fb314593580b93f8293d5d7340ac587196ae6544
VarunSoni01 <varunsoni448@gmail.com>
2025-10-22 10:33:20 +0530

    checkpoint

--- src/main/java/com/example/billingsoftware/BillingSoftwareBackend/controller/PaymentController.java
+++ src/main/java/com/example/billingsoftware/BillingSoftwareBackend/controller/PaymentController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/payments")
 @RequiredArgsConstructor
+@CrossOrigin("*") //will accepts requests all origin
 public class PaymentController {
 
     private final RazorpayService razorpayService;
